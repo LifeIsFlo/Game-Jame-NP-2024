0651505c74f2559e998ad160158754a99983b8e1
Florian Cornelisse <FlorianCornelisse@outlook.com>
2024-02-13 15:27:14 +0100

    Commit

--- Project/Assets/Scripts/Sack/SackBullet.cs
+++ Project/Assets/Scripts/Sack/SackBullet.cs
@@ -6,10 +6,13 @@ public class SackBullet : MonoBehaviour
 {
     private void OnEnable()
     {
-        Destroy(gameObject, 10f);
+        Destroy(gameObject, 2);
     }
     private void OnCollisionEnter(Collision collision)
     {
-        Destroy(gameObject);
+        Destroy(GetComponent<MeshFilter>());
+        Destroy(GetComponent<MeshRenderer>());
+        Destroy(GetComponent<Rigidbody>());
+        transform.GetChild(0).gameObject.SetActive(true);
     }
 }
