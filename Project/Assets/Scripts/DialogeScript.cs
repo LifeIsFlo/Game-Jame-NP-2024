13e6e5bd64a4e33a4b0d061bee683a484516f2d0
RudoVerschuren <rpverschuren@st.noorderpoort.nl>
2024-02-13 14:32:32 +0100

    Comments

--- Project/Assets/Scripts/DialogeScript.cs
+++ Project/Assets/Scripts/DialogeScript.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 
 public class DialogeScript : MonoBehaviour
 {
-    public AudioClip testAudio;
+    //Objects n stuff
     [SerializeField] private GameObject audioSource;
     [SerializeField] private GameObject dialogeBox;
     [SerializeField] private TMP_Text dialogeText;
     [SerializeField] private TMP_Text nameText;
+
+    //Doing the dialoge stuffs
     private string[] currentText;
     private string[] currentNames;
     private AudioClip[] currentAudio;
@@ -19,9 +21,11 @@ public class DialogeScript : MonoBehaviour
     private GameObject currentSource;
     private GameObject sourceEmpty;
     bool canSkip;
-    // Start is called before the first frame update
+
+
     void Start()
     {
+        //Adds some objects if they dont exist
         if(dialogeBox == null)
         {
             dialogeBox = GameObject.Find("Dialoge");
@@ -38,22 +42,30 @@ public class DialogeScript : MonoBehaviour
         {
             sourceEmpty = GameObject.Find("AudioSources");
         }
+        //Turns the dialoge box off because otherwise it cant find it
         dialogeBox.SetActive(false);
-        PlayDialoge(new string[] {"Dia1","Dia2","Dia3","\"Test Audiok\"","Dia5","Dia6"},new string[] {"Name1","Name2","Name3","Name4","Name5","Name6"},new AudioClip[] {testAudio,null,testAudio,testAudio,testAudio,testAudio}, new float[] {30f,10,10,10,10,10});
+
+        //This how you call these things
+
+        //PlayDialoge(new string[] {"Dia1","Dia2","Dia3","\"Test Audiok\"","Dia5","Dia6"},new string[] {"Name1","Name2","Name3","Name4","Name5","Name6"},new AudioClip[] {testAudio,null,testAudio,testAudio,testAudio,testAudio}, new float[] {30f,10,10,10,10,10});
         //PlayVoiceline(testAudio,true);
     }
 
     private void Update()
     {
+        //Skip dialoge
         if (Input.GetButtonDown("SkipDialoge") && canSkip)
         {
             Debug.Log("Skipped Dialoge");
             timeTillNextDial = -1;
             Destroy(currentSource);
         }
+
+        //Otherwise you will get an error if you dont have any dialoge
         if (currentAudio != null)
         {
             timeTillNextDial -= Time.deltaTime;
+            //Does an array of dialoges
             if (currentIndex < currentAudio.Length)
             {
                 if (timeTillNextDial < 0)
@@ -69,6 +81,7 @@ public class DialogeScript : MonoBehaviour
             }
             else
             {
+                //Turns off the dialoge box if dialoge is done
                 if (timeTillNextDial < 0)
                 {
                     dialogeBox.SetActive(false);
@@ -79,6 +92,7 @@ public class DialogeScript : MonoBehaviour
 
     public void PlayDialoge(string[] text, string[] names, AudioClip[] audio, float[] dialogeTime)
     {
+        //Basically sets all the values
         currentIndex = 0;
         currentText = text;
         currentNames = names;
@@ -88,11 +102,13 @@ public class DialogeScript : MonoBehaviour
     }
     public void PlayText(string text,string name)
     {
+        //Sets the text
         dialogeText.text = text;
         nameText.text = name;
     }
     public void PlayVoiceline(AudioClip audio,bool skippable)
     {
+        //Spawns an audio source and makes it play stuff
         if(audioSource != null)
         {
             AudioSource source = Instantiate(audioSource).GetComponent<AudioSource>();
