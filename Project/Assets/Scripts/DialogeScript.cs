d7529bb12738fa526d0a507a7540f8a1eb1a7041
RudoVerschuren <rpverschuren@st.noorderpoort.nl>
2024-02-13 14:28:50 +0100

    You can make unskippable audio, and also can not have audio

--- Project/Assets/Scripts/DialogeScript.cs
+++ Project/Assets/Scripts/DialogeScript.cs
@@ -18,6 +18,7 @@ public class DialogeScript : MonoBehaviour
     private float[] currentDialogeTimes;
     private GameObject currentSource;
     private GameObject sourceEmpty;
+    bool canSkip;
     // Start is called before the first frame update
     void Start()
     {
@@ -37,33 +38,41 @@ public class DialogeScript : MonoBehaviour
         {
             sourceEmpty = GameObject.Find("AudioSources");
         }
-        PlayDialoge(new string[] {"Dia1","Dia2","Dia3","\"Test Audiok\"","Dia5","Dia6"},new string[] {"Name1","Name2","Name3","Name4","Name5","Name6"},new AudioClip[] {testAudio,testAudio,testAudio,testAudio,testAudio,testAudio}, new float[] {10f,10,10,10,10,10});
+        dialogeBox.SetActive(false);
+        PlayDialoge(new string[] {"Dia1","Dia2","Dia3","\"Test Audiok\"","Dia5","Dia6"},new string[] {"Name1","Name2","Name3","Name4","Name5","Name6"},new AudioClip[] {testAudio,null,testAudio,testAudio,testAudio,testAudio}, new float[] {30f,10,10,10,10,10});
+        //PlayVoiceline(testAudio,true);
     }
 
     private void Update()
     {
-        if (Input.GetButtonDown("SkipDialoge"))
+        if (Input.GetButtonDown("SkipDialoge") && canSkip)
         {
             Debug.Log("Skipped Dialoge");
             timeTillNextDial = -1;
             Destroy(currentSource);
         }
-        timeTillNextDial -= Time.deltaTime;
-        if (currentIndex < currentAudio.Length)
+        if (currentAudio != null)
         {
-            if (timeTillNextDial < 0)
+            timeTillNextDial -= Time.deltaTime;
+            if (currentIndex < currentAudio.Length)
             {
-                PlayText(currentText[currentIndex], currentNames[currentIndex]);
-                PlayVoiceline(currentAudio[currentIndex]);
-                timeTillNextDial = currentDialogeTimes[currentIndex];
-                currentIndex++;
+                if (timeTillNextDial < 0)
+                {
+                    PlayText(currentText[currentIndex], currentNames[currentIndex]);
+                    if (currentAudio[currentIndex] != null)
+                    {
+                        PlayVoiceline(currentAudio[currentIndex], true);
+                    }
+                    timeTillNextDial = currentDialogeTimes[currentIndex];
+                    currentIndex++;
+                }
             }
-        }
-        else
-        {
-            if (timeTillNextDial < 0)
+            else
             {
-                dialogeBox.SetActive(false);
+                if (timeTillNextDial < 0)
+                {
+                    dialogeBox.SetActive(false);
+                }
             }
         }
     }
@@ -82,7 +91,7 @@ public class DialogeScript : MonoBehaviour
         dialogeText.text = text;
         nameText.text = name;
     }
-    public void PlayVoiceline(AudioClip audio)
+    public void PlayVoiceline(AudioClip audio,bool skippable)
     {
         if(audioSource != null)
         {
@@ -92,6 +101,7 @@ public class DialogeScript : MonoBehaviour
             source.gameObject.GetComponent<TimeKillScript>().currentTime = audio.length;
             currentSource = source.gameObject;
             currentSource.transform.parent = sourceEmpty.transform;
+            canSkip = skippable;
         }
     }
 }
